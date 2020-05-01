de74bd562fd862f85d8551cb57de25b561e14372
Victor Sibanda <victormsibanda1@gmail.com>
2020-05-01 11:02:56 +0100

    Target Group

--- modules/app_tier/main.tf
+++ modules/app_tier/main.tf
@@ -225,7 +225,7 @@ resource "aws_launch_configuration" "app_launchconfig" {
   image_id        = var.ami_id
   instance_type   ="t2.micro"
   security_groups = [aws_security_group.App_SG.id]
-  associate_public_ip_address = true
+  associate_public_ip_address = false
   key_name = "victor-eng54"
   user_data = data.template_file.app_init.rendered
   lifecycle {
